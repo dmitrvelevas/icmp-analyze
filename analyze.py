75d248f4b4c990546dafc3a33dd624a53d9f2fcc
Saveliev Dmitriy <saveliev@bubuka.info>
2024-04-26 17:17:11 +0700

    исправлена ошибка в analyze, добавлены проверки

--- analyze.py
+++ analyze.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
-import scapy.all as scapy
-
 #----Настраиваемые параметры----
-dump_file_A = "A.dump"
-dump_file_B = "B.dump"
 
 ip_A='10.249.249.1'
 ip_B='10.249.249.242'
+
+dump_file_A = "A.dump"
+dump_file_B = "B.dump"
+
 #------------------------------
+
+import scapy.all as scapy
 
 dump_A = scapy.rdpcap(dump_file_A)
 dump_B = scapy.rdpcap(dump_file_B)
@@ -21,14 +23,24 @@ def one_direction_time(dump1, dump2, src, dst):
     result = []
 
     for i in range(0, len(dump1)-1):
-        #отфильтровываем только icmp трафик и пакеты, осносящиеся к взаимодействию исследуемой пары хостов
+    
+        #отбрасываем не ip пакеты
+        if "IP" not in dump1[i]:
+            continue
+        
+        #оставляем только icmp пакеты, осносящиеся к взаимодействию исследуемой пары хостов
         if  dump1[i]['IP'].src == src and dump1[i]['IP'].dst == dst and dump1[i]['IP'].proto == 1:
             for j in range(0, len(dump2)-1):
-                #отфильтровываем только icmp трафик и пакеты, осносящиеся к взаимодействию исследуемой пары хостов
+                
+                #отбрасываем не ip пакеты
+                if "IP" not in dump2[j]:
+                    continue
+                
+                #оставляем только icmp пакеты, относящиеся к взаимодействию исследуемой пары хостов
                 if  dump2[j]['IP'].src == src and dump2[j]['IP'].dst == dst and dump2[j]['IP'].proto == 1:
                     #находим пакеты с одинаковой контрольной суммой и вычисляем время между отправкой и получением
                     if dump1[i]['ICMP'].chksum == dump2[j]['ICMP'].chksum:
-                        result.append(round(abs(dump_A[i].time - dump_B[j].time)*1000))
+                        result.append(round(abs(dump1[i].time - dump2[j].time)*1000))
     
     return result
 
